55054d314bcc7988310c994d6c7586bec84bfa21
James Cook <james.cook@utoronto.ca>
2012-02-29 00:24:40 -0800

    Update offlineimap.

--- pkgs/tools/networking/offlineimap/default.nix
+++ pkgs/tools/networking/offlineimap/default.nix
@@ -1,16 +1,26 @@
-{ fetchurl, buildPythonPackage, ssl }:
+{ fetchgit, buildPythonPackage, ssl }:
 
 buildPythonPackage {
-  name = "offlineimap-6.2.0.2";
+  name = "offlineimap-6.4.0";
 
-  src = fetchurl {
-    url = "http://ftp.de.debian.org/debian/pool/main/o/offlineimap/offlineimap_6.2.0.2.orig.tar.gz";
-    sha256 = "1w69qv1dm37m53k8cd068lk5z3qjlscnjxr397gs8kdsfds67v7c";
+  src = fetchgit {
+    url = "https://github.com/nicolas33/offlineimap.git";
+    rev = "c1120c9158f273fe691f89b9eefc34b2623d5a7a";  # 6.4.0 release
   };
 
   propagatedBuildInputs = [ ssl ];
 
   doCheck = false;
+
+  # Copied from applications/graphics/jbrout, to avoid the following error message:
+  #   File "/nix/store/vzpvrymynp4n93bznxha6hadj0ww68vx-python-2.7.1/lib/python2.7/zipfile.py", line 348, in FileHeader
+  #     len(filename), len(extra))
+  # struct.error: ushort format requires 0 <= number <= USHRT_MAX
+  preConfigure = ''
+    find | xargs touch
+  '';
+
+  patches = [ ./detect_ssl.patch ];
 
   meta = {
     description = "IMAP to local files bridge";
